997fc0a3ecec0b395ca82b7543f10a11b8ca8bb9
Sardar Manpreet Singh <64973354+manpreet147@users.noreply.github.com>
2026-07-23 01:01:10 +0530

    Enhance Employee component to display detailed employee information with improved styling, including name, designation, department, attendance status, and working hours.

--- Kudrath front/src/pages/Employee.js
+++ Kudrath front/src/pages/Employee.js
@@ -19,9 +19,24 @@ const Employees = () => {
         <div style = {{padding: "20px"}}>
             <h1>Employee List</h1>
 
-            {employees.map(emp) => (
-                <div  
-            )}
+            {employees.map((emp) => (
+                <div 
+                    key = {emp.id}
+                    style={{
+                        border: "1px solid grey",
+                        padding: "10px",
+                        marginBottom: "10px"
+                    }}>
+                    <h3>{emp.employee_name}</h3>
+                    <p><b>Designation:</b>{emp.designation}</p>
+                    <p><b>Department:</b>{emp.department}</p>
+                    <p><b>status:</b>{emp.attendancestatus}</p>
+                    <p><b>Working Hours:</b>{emp.attendance.working_hours}</p>
+            
+                </div>
+            ))}
         </div>
     )
-}
+};
+
+export default Employees;
